52d3ae969d98ad4ee0c651298bc338201ed952e5
turnerisabel <turner.isabel03@gmail.com>
2021-06-01 12:37:33 +0200

    updated Test 010

--- src/main/java/at/htl/recap/VehicleRepository.java
+++ src/main/java/at/htl/recap/VehicleRepository.java
@@ -4,8 +4,10 @@ import at.htl.recap.entity.Vehicle;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
+@Transactional
 public class VehicleRepository implements PanacheRepository<Vehicle> {
 
 }
--- src/test/java/at/htl/recap/VehicleRepositoryTest.java
+++ src/test/java/at/htl/recap/VehicleRepositoryTest.java
@@ -1,11 +1,15 @@
 package at.htl.recap;
 
 import at.htl.recap.entity.Vehicle;
+import io.agroal.api.AgroalDataSource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.assertj.db.output.Outputs.output;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
@@ -14,11 +18,22 @@ class VehicleRepositoryTest {
     @Inject
     VehicleRepository vehicleRepository;
 
+    @Inject
+    AgroalDataSource ds;
+
     @Test
     void t010_insertSimpleVehicle() {
 
         Vehicle vehicle = new Vehicle("Opel", "Kapitän", 1990);
         vehicleRepository.persist(vehicle);
+
+        Table table = new Table(ds, "V_VEHICLE");
+        output(table).toConsole();
+        assertThat(table).hasNumberOfRows(1)
+                .row()
+                .column("V_ID").value().isGreaterThanOrEqualTo(1000)
+                .column("V_BRAND").value().isEqualTo("Opel")
+                .column("V_Model").value().isEqualTo("Kapitän");
 
     }
 
