c969aa5f41e46e734ed3f9bf841670a51227d66d
turnerisabel <turner.isabel03@gmail.com>
2021-06-01 12:07:59 +0200

    initial commit

---
+++ src/main/java/at/htl/recap/VehicleRepository.java
@@ -0,0 +1,11 @@
+package at.htl.recap;
+
+import at.htl.recap.entity.Vehicle;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class VehicleRepository implements PanacheRepository<Vehicle> {
+
+}
---
+++ src/main/java/at/htl/recap/entity/Vehicle.java
@@ -0,0 +1,40 @@
+package at.htl.recap.entity;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "V_VEHICLE")
+@SequenceGenerator(name = "vehicleSeq",
+        sequenceName = "VEHICLE_SEQ",
+        initialValue = 1000)
+public class Vehicle extends PanacheEntityBase {
+
+    @Id
+    @Column(name = "V_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "vehicleSeq"
+    )
+    Long id;
+
+    @Column(name = "V_BRAND")
+    String brand;
+
+    @Column(name = "V_MODEL")
+    String model;
+
+    @Column(name = "V_CONSTRUCTION_YEAR")
+    int constructionYear;
+
+    public int age(){
+        return LocalDate.now().getYear() - constructionYear;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", brand, model);
+    }
+}
