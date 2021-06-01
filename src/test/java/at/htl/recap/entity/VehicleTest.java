3d6f207a42b38e66d3de0d592dca24499f0f8835
turnerisabel <turner.isabel03@gmail.com>
2021-06-01 12:41:06 +0200

    VehicleTest2

---
+++ src/test/java/at/htl/recap/entity/VehicleTest.java
@@ -0,0 +1,14 @@
+package at.htl.recap.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    void t010_checkAgeMethod(){
+        Vehicle vehicle = new Vehicle("VW", "Käfer", 1969);
+    }
+
+}
